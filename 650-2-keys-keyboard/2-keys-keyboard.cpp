548e7e9a2a768222e2189a25b7e49e4dec4c7948
SatyamJais136 <167278982+SatyamJais136@users.noreply.github.com>
2026-01-18 10:19:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (95.20%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool isPrime(int n){
         if(n == 0 || n == 1) return false;
-        for(int i = 2; i  <= sqrt(n); i++){
+        for(int i = 2; i*i  <= n; i++){
             if(n%i == 0) return false;
         }
         return true;
     }
 
     int gd(int n){
-        for(int i = 2; i <= sqrt(n); i++){
+        for(int i = 2; i*i <= n; i++){
             if(n%i == 0) return (n/i); 
         }
         return 1;
