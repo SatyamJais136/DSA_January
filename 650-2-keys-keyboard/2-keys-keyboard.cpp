ebb12f90117e441932230a54be4065f3cba65721
SatyamJais136 <167278982+SatyamJais136@users.noreply.github.com>
2026-01-18 10:13:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (95.20%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPrime(int n){
+        if(n == 0 || n == 1) return false;
+        for(int i = 2; i  <= sqrt(n); i++){
+            if(n%i == 0) return false;
+        }
+        return true;
+    }
+
+    int gd(int n){
+        for(int i = 2; i <= sqrt(n); i++){
+            if(n%i == 0) return (n/i); 
+        }
+        return 1;
+    }
+
+    int minSteps(int n) {
+        int  cnt = 0;
+        while(n > 1){
+            if(isPrime(n)){
+                cnt += n;
+                break;
+            }
+            int hf = gd(n);
+            cnt += (n/hf);
+            n = hf;
+        }
+        return cnt;
+    }
+};
