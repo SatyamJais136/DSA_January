e4a3be29dffab60433e8a3d4b5837613cfa0900a
SatyamJais136 <167278982+SatyamJais136@users.noreply.github.com>
2026-01-22 20:22:04 +0530

    Time: 4 ms (10.10%) | Memory: 12.8 MB (25.83%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    void helper(int idx, vector<int> &nums, vector<int> &cur, vector<vector<int>> &res){
-        res.push_back(cur);
-        for(int i = idx; i < nums.size(); i++){
-            cur.push_back(nums[i]);
-            helper(i+1, nums, cur, res);
-            cur.pop_back();
-        }
+void solve(int idx, vector<int>& nums, vector<int>& curr,vector<vector<int>>& ans) {
+    if (idx == nums.size()) {
+        ans.push_back(curr);
+        return;
     }
 
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> cur;
-        helper(0, nums, cur, res);
-        return res;
-    }
+    // Exclude nums[idx]
+    solve(idx + 1, nums, curr, ans);
+
+    // Include nums[idx]
+    curr.push_back(nums[idx]);
+    solve(idx + 1, nums, curr, ans);
+    curr.pop_back();
+}
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> ans;
+    vector<int> curr;
+    solve(0, nums, curr, ans);
+    return ans;
+}
+
 };
