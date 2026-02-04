0d0ad6bcdc5b2ec2eb705cf3fe33236f6244fd66
SatyamJais136 <167278982+SatyamJais136@users.noreply.github.com>
2026-02-04 18:17:41 +0530

    Time: 221 ms (8.21%) | Memory: 205.5 MB (6.89%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool rec(int i, int sum, int target, int n, vector<int> &nums, vector<vector<int>> &dp){
+        if(n == i) return (target == sum);
+        if(sum > target) return false;
+        if(dp[i][sum] != -1) return dp[i][sum];
+        int take = rec(i+1, sum + nums[i], target, n, nums, dp);
+        int notTake = rec(i+1, sum, target, n, nums, dp);
+        return dp[i][sum] = (take||notTake);
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int total = 0;
+        for(int i = 0; i < n; i++){
+            total += nums[i];
+        }
+        if(total % 2 != 0) return false;
+        int target = total/2;
+        vector<vector<int>> dp(n+1, vector<int>(total+1, -1));
+        return rec(0, 0, target, n, nums, dp);
+    }
+};
